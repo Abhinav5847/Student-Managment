058b222d604b16de295332721674247595420067
Abhinav <abnvv07@gmail.com>
2026-01-16 13:02:53 +0530

    few updates

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("students/",views.student,name='student'),
+    path("",views.student,name='student'),
     path('login/',views.login_view,name='login'),
-    path('register/',views.register_view,name='register')
+    path('register/',views.register_view,name='register'),
+    path('logout/',views.logout_view,name='logout')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
 
 def student(request):
    return render(request,'students.html')
 
 def register_view(request):
    if request.method == 'POST':
-      username = request.POST['username']
-      email = request.POST['email']
-      password1 = request.POST['password1']
-      password2 = request.POST['password2']
+      username = request.POST.get('username')
+      email = request.POST.get('email')
+      password1 = request.POST.get('password1')
+      password2 = request.POST.get('password2')
 
       if password1 != password2:
           messages.error(request,"password doest not match")
@@ -27,9 +29,29 @@ def register_view(request):
       ) 
 
       messages.success(request,'registration completed')
+      return redirect('login')
 
-      return render(request,'login.html')  
+   return render(request,'register.html')
+  
+
+def login_view(request):
+   if request.method == 'POST':
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+
+      user = authenticate(request,username=username,password=password)
+
+      if user:
+         login(request,user)
+         return redirect('student')
+      else:
+         messages.error(request,'Invalid credentials')
+
+   return render(request,'login.html')      
+
+def logout_view(request):
+   
+
       
 
 
-# Create your views here.
