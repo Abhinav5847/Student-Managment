47d77b0afe267048322d661f8d2768990016e97d
Abhinav <abnvv07@gmail.com>
2026-01-16 11:50:52 +0530

    feat: add user sign form

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("students/",views.student,name='student'),
     path('login/',views.login_view,name='login'),
-    path('logout/',views.logout_view,name='logout')
+    path('register/',views.register_view,name='register')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def student(request):
    return render(request,'students.html')
+
+def register_view(request):
+   if request.method == 'POST':
+      username = request.POST['username']
+      email = request.POST['email']
+      password1 = request.POST['password1']
+      password2 = request.POST['password2']
+
+      if password1 != password2:
+          messages.error(request,"password doest not match")
+          return redirect('register')
+      
+      if User.objects.filter(username=username).exists():
+         messages.error(request,'username is already exit!')
+         return redirect('register')
+
+      User.objects.create_user(
+         username=username,
+         email=email,
+         password=password1
+      ) 
+
+      messages.success(request,'registration completed')
+
+      return render(request,'login.html')  
+      
+
 
 # Create your views here.
